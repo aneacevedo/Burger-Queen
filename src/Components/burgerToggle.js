8e3de5e27c6ddc065999e5cd1082a3595976f279
Paupito <paula.frias@ug.uchile.cl>
2020-09-01 08:58:21 -0400

    tercer vista

--- src/Components/burgerToggle.js
+++ src/Components/burgerToggle.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
+import InitialPage from './initialPage.js';
 
+const activeToggle = () => document.querySelector('#contentToggle').classList.toggle('active');
 function BurgerToggle() {
     return ( 
-            <div id="contentToggle">
+        <div className='sideBar'>
+            <div id="contentToggle" onClick={activeToggle}>
                 <p className="toggle">&#9776;</p>
             </div>
+{/* ingresar componente thirdPage */}
+          <div>
+              <p>Mesas</p>
+          </div>
+
+          <div>
+              <p>Pedidos</p>
+          </div>
+{/* ingresar componente initialpage */}
+          <div onClick={InitialPage}> 
+              <p>Cambiar cuenta</p>
+          </div>
+            
+         </div>
     );
 }
 
