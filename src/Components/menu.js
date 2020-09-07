20cab9f8ad425d9eccba7dc3b0be7da089790786
Paupito <paula.frias@ug.uchile.cl>
2020-09-06 23:01:30 -0300

    recorrer y visualizar resto de la data, botones menuPage funcionables

--- src/Components/menu.js
+++ src/Components/menu.js
@@ -1,27 +1,94 @@
 import React from 'react';
-// import ShowBreakClub from './showBreakClub.js';
+import ShowMenuTradi from './showMenusTradi';
+import { withRouter } from 'react-router'
+import ShowBreakfast from './showBreakClub';
 
 
-// const showB = (e) => { 
-//     e.preventDefault();
-//     console.log(ShowBreakClub);
-// }
+class MenuPage extends React.Component {
 
-function MenuPage() {
-    return ( 
+ constructor(){
+   super();
 
-        <div className='containerMenuPage'> 
-            <div className='tableAndName'>
-                <p className='title'>nombredemesa</p>
-                <input type='text' className='nameComensal' placeholder='Ingresar nombre'/>
+   this.state ={
+    showMenuClub: false,
+  }
+  this.showMenuClub = this.showMenuClub.bind(this);
+ }
+ showMenuClub(event){
+   event.preventDefault();
+
+   this.setState({
+     showMenuClub: !this.state.showMenuClub,
+   });
+ }
+
+ render () {
+  return ( 
+    <div className='containerMenuPage'> 
+      <div className='leftSideMenuPage'>  
+        <header className='headerMenu'>
+      
+         <div className='tableAndName'>
+           <p className='titleM'>Nombredemesa</p>
+           <input type='text' className='nameComensal' placeholder='Ingresar nombre'/>
+          </div>
+          <div className='containerBtnsMenus'>
+             <button className='tMenu' onClick={this.showMenuClub}>Menú Tradicional</button>
+             <button className='tMenu' onClick={this.showMenuClub}>Breakfast Club</button>
+          </div>
+        </header>
+        <div className="containerShowMenu"> 
+          {
+            this.state.showMenuClub
+            ? (
+              <ShowBreakfast/>
+            )
+            : (
+              <ShowMenuTradi/>
+            )
+          }
+        
+        </div>
+        
+      </div>
+      <div className='rightSideMenuPage'> 
+
+       <div className='orderContainer'>
+         <div className='textOrder'> 
+            <div className='titleAndImg'>
+              <p className='titleOrder'>Pedido</p>
+              <img src='../media/trash.png' alt='trash'/>
             </div>
-            <div>
-                <button className='tMenu'>Menú Tradicional</button>
-                {/* <button className='tMenu' onClick={showB()}>Breakfast Club</button> */}
-            </div>
+            <div className='orderPicksContainer'>
+              <p>ordenes</p>
+            </div>  
+         </div>
         </div>
 
-    );
+
+        <div className='containerTipsAndBtn'>
+          <div>
+            <textarea placeholder='Añadir notas al chef'></textarea>
+          </div>
+          <div className='containerTips'>
+            <p className='titleTips'>Propina</p>
+            <button className='btnTips'>$500</button>
+            <button className='btnTips'>$1000</button>
+            <button className='btnTips'>$1500</button>
+            <button className='btnTips'>$2000</button>
+          </div>
+          <div className='containerPriceBtn'>
+            <p className='totalPrice'>Total $</p>
+            <button className='aceptBtn'>CONFIRMAR PEDIDO</button>
+          </div>
+         </div>
+
+       </div>    
+    </div>
+
+  );
+ }
+   
 }
 
-export default MenuPage;
+export default withRouter(MenuPage);
